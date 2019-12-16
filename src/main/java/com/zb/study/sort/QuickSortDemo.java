072d66d6f566f778861380f18751308743d4b820
zhangbo <zhangbo@zsyjr.com>
2019-12-16 20:11:56 +0800

    快速排序

---
+++ src/main/java/com/zb/study/sort/QuickSortDemo.java
@@ -0,0 +1,47 @@
+package com.zb.study.sort;
+
+/**
+ * @author Zhang Bo
+ * @date 2019/12/16 19:41
+ */
+public class QuickSortDemo extends BaseSort {
+    public static void main(String[] args) {
+        QuickSortDemo quick = new QuickSortDemo();
+        int[] arr = generateArr(10);
+        printArr(arr);
+        long startTime = System.currentTimeMillis();
+        int[] newArr = quick.quickSort(arr);
+        System.out.println("time:" + (System.currentTimeMillis() - startTime));
+        printArr(newArr);
+    }
+
+    public int[] quickSort(int[] arr) {
+        quickSort(arr, 0, arr.length - 1);
+        return arr;
+    }
+
+    private void quickSort(int[] arr, int l, int r) {
+        if (l >= r) {
+            return;
+        }
+        int p = quick(arr, l, r);
+        quickSort(arr, l, p - 1);
+        quickSort(arr, p + 1, r);
+    }
+
+    private int quick(int[] arr, int l, int r) {
+        int j = l;
+        for (int i = l; i < r; i++) {
+            if (arr[i] < arr[r]) {
+                int tmp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = tmp;
+                j++;
+            }
+        }
+        int tmp = arr[r];
+        arr[r] = arr[j];
+        arr[j] = tmp;
+        return j;
+    }
+}
