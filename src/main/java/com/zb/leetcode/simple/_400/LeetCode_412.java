d8417362f21730f2e11a36d1f6d67bf4ca9948ba
张博 <zhangbo@zsyjr.com>
2020-06-05 19:02:44 +0800

    leet code 412

---
+++ src/main/java/com/zb/leetcode/simple/_400/LeetCode_412.java
@@ -0,0 +1,70 @@
+package com.zb.leetcode.simple._400;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 412. Fizz Buzz
+ *
+ * @author Zhang Bo
+ * @date 2020/6/2 11:05
+ */
+public class LeetCode_412 {
+
+    /**
+     * 写一个程序，输出从 1 到 n 数字的字符串表示。
+     * <p>
+     * 1. 如果 n 是3的倍数，输出“Fizz”；
+     * <p>
+     * 2. 如果 n 是5的倍数，输出“Buzz”；
+     * <p>
+     * 3.如果 n 同时是3和5的倍数，输出 “FizzBuzz”。
+     * <p>
+     * 示例：
+     * <p>
+     * n = 15,
+     * <p>
+     * 返回:
+     * [
+     * "1",
+     * "2",
+     * "Fizz",
+     * "4",
+     * "Buzz",
+     * "Fizz",
+     * "7",
+     * "8",
+     * "Fizz",
+     * "Buzz",
+     * "11",
+     * "Fizz",
+     * "13",
+     * "14",
+     * "FizzBuzz"
+     * ]
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/fizz-buzz
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    public static void main(String[] args) {
+        LeetCode_412 leetCode412 = new LeetCode_412();
+        System.out.println(leetCode412.fizzBuzz(15));
+    }
+
+    public List<String> fizzBuzz(int n) {
+        List<String> result = new ArrayList<>(n);
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                result.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                result.add("Fizz");
+            } else if (i % 5 == 0) {
+                result.add("Buzz");
+            } else {
+                result.add(String.valueOf(i));
+            }
+        }
+        return result;
+    }
+}
