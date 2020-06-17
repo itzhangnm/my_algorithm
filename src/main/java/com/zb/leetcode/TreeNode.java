5aa1d777493db0ed55e8153136243f15b7ab8109
张博 <zhangbo@zsyjr.com>
2020-06-17 17:07:06 +0800

    leetcode 101

---
+++ src/main/java/com/zb/leetcode/TreeNode.java
@@ -0,0 +1,15 @@
+package com.zb.leetcode;
+
+/**
+ * @author Zhang Bo
+ * @date 2020/6/8 18:25
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int x) {
+        val = x;
+    }
+}
