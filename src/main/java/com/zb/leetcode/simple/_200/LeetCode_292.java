99920b60ebef54fcacfa30117e926b8dc8e33177
once <itzhangnima@163.com>
2020-09-30 21:36:39 +0800

    leetcode 292

---
+++ src/main/java/com/zb/leetcode/simple/_200/LeetCode_292.java
@@ -0,0 +1,13 @@
+package com.zb.leetcode.simple._200;
+
+/***
+ * 292. Nim 游戏
+ * @author once
+ * @date 2020/9/30 21:27
+ *
+ */
+public class LeetCode_292 {
+    public boolean canWinNim(int n) {
+        return n % 4 != 0;
+    }
+}
