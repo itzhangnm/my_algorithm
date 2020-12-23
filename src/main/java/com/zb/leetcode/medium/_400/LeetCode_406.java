afb603adfc366700659f9553c5796e701a22bd2a
once <itzhangnima@163.com>
2020-12-23 23:09:23 +0800

    leetcode 406

---
+++ src/main/java/com/zb/leetcode/medium/_400/LeetCode_406.java
@@ -0,0 +1,69 @@
+package com.zb.leetcode.medium._400;
+
+import com.alibaba.fastjson.JSON;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/***
+ * 406. 根据身高重建队列
+ * @author once
+ * @date 2020/12/22 23:44
+ *
+ */
+public class LeetCode_406 {
+    /**
+     * 假设有打乱顺序的一群人站成一个队列，数组 people 表示队列中一些人的属性（不一定按顺序）。每个 people[i] = [hi, ki] 表示第 i 个人的身高为 hi ，前面 正好 有 ki 个身高大于或等于 hi 的人。
+     * <p>
+     * 请你重新构造并返回输入数组 people 所表示的队列。返回的队列应该格式化为数组 queue ，其中 queue[j] = [hj, kj] 是队列中第 j 个人的属性（queue[0] 是排在队列前面的人）。
+     * <p>
+     *  
+     * <p>
+     * 示例 1：
+     * <p>
+     * 输入：people = [[7,0],[4,4],[7,1],[5,0],[6,1],[5,2]]
+     * 输出：[[5,0],[7,0],[5,2],[6,1],[4,4],[7,1]]
+     * 解释：
+     * 编号为 0 的人身高为 5 ，没有身高更高或者相同的人排在他前面。
+     * 编号为 1 的人身高为 7 ，没有身高更高或者相同的人排在他前面。
+     * 编号为 2 的人身高为 5 ，有 2 个身高更高或者相同的人排在他前面，即编号为 0 和 1 的人。
+     * 编号为 3 的人身高为 6 ，有 1 个身高更高或者相同的人排在他前面，即编号为 1 的人。
+     * 编号为 4 的人身高为 4 ，有 4 个身高更高或者相同的人排在他前面，即编号为 0、1、2、3 的人。
+     * 编号为 5 的人身高为 7 ，有 1 个身高更高或者相同的人排在他前面，即编号为 1 的人。
+     * 因此 [[5,0],[7,0],[5,2],[6,1],[4,4],[7,1]] 是重新构造后的队列。
+     * 示例 2：
+     * <p>
+     * 输入：people = [[6,0],[5,0],[4,0],[3,2],[2,2],[1,4]]
+     * 输出：[[4,0],[5,0],[2,2],[3,2],[1,4],[6,0]]
+     *  
+     * <p>
+     * 提示：
+     * <p>
+     * 1 <= people.length <= 2000
+     * 0 <= hi <= 106
+     * 0 <= ki < people.length
+     * 题目数据确保队列可以被重建
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/queue-reconstruction-by-height
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    public static void main(String[] args) {
+        LeetCode_406 leetCode_406 = new LeetCode_406();
+        System.out.println(JSON.toJSONString(leetCode_406.reconstructQueue(new int[][]{{6, 0}, {5, 0}, {4, 0}, {3, 2}, {2, 2}, {1, 4}})));
+        System.out.println(JSON.toJSONString(leetCode_406.reconstructQueue(new int[][]{{7, 0}, {4, 4}, {7, 1}, {5, 0}, {6, 1}, {5, 2}})));
+//        [[3,0],[6,0],[7,0],[5,2],[3,4],[5,3],[6,2],[2,7],[9,0],[1,9]]
+//        [[6,0],[7,0],[5,2],[6,2],[9,0],[3,0],[3,4],[2,7],[5,3],[1,9]]
+        System.out.println(JSON.toJSONString(leetCode_406.reconstructQueue(new int[][]{{9, 0}, {7, 0}, {1, 9}, {3, 0}, {2, 7}, {5, 3}, {6, 0}, {3, 4}, {6, 2}, {5, 2}})));
+    }
+
+    public int[][] reconstructQueue(int[][] people) {
+        Arrays.sort(people, (int[] a, int[] b) -> (a[0] == b[0] ? a[1] - b[1] : b[0] - a[0]));
+        List<int[]> list = new ArrayList<>();
+        for (int[] person : people) {
+            list.add(person[1],person);
+        }
+        return list.toArray(new int[list.size()][]);
+    }
+}
