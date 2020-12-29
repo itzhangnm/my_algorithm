9846a6456994fe1ba39031a3300f5f3fa538c905
once <itzhangnima@163.com>
2020-12-29 23:28:10 +0800

    leetcode 148

---
+++ src/main/java/com/zb/leetcode/medium/_100/LeetCode_148.java
@@ -0,0 +1,96 @@
+package com.zb.leetcode.medium._100;
+
+import com.alibaba.fastjson.JSON;
+import com.zb.leetcode.ListNode;
+
+/***
+ * 148. 排序链表
+ * @author once
+ * @date 2020/12/29 22:30
+ *
+ */
+public class LeetCode_148 {
+    /**
+     * 给你链表的头结点 head ，请将其按 升序 排列并返回 排序后的链表 。
+     *
+     * 进阶：
+     *
+     * 你可以在 O(n log n) 时间复杂度和常数级空间复杂度下，对链表进行排序吗？
+     *  
+     *
+     * 示例 1：
+     *
+     *
+     * 输入：head = [4,2,1,3]
+     * 输出：[1,2,3,4]
+     * 示例 2：
+     *
+     *
+     * 输入：head = [-1,5,3,4,0]
+     * 输出：[-1,0,3,4,5]
+     * 示例 3：
+     *
+     * 输入：head = []
+     * 输出：[]
+     *  
+     *
+     * 提示：
+     *
+     * 链表中节点的数目在范围 [0, 5 * 104] 内
+     * -105 <= Node.val <= 105
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/sort-list
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    public static void main(String[] args) {
+        LeetCode_148 leetCode_148 = new LeetCode_148();
+        ListNode head = new ListNode(4);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(1);
+        head.next.next.next = new ListNode(3);
+        ListNode listNode = leetCode_148.sortList(head);
+        System.out.println(JSON.toJSONString(listNode));
+
+    }
+
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode left = head;
+        ListNode right = slow.next;
+        slow.next = null;
+        left = sortList(left);
+        right = sortList(right);
+        ListNode rs = new ListNode(-1);
+        ListNode temp = rs;
+        while (left != null && right != null) {
+            if (left.val <= right.val) {
+                temp.next = left;
+                left = left.next;
+            }else {
+                temp.next = right;
+                right = right.next;
+            }
+            temp = temp.next;
+        }
+        while (left != null) {
+            temp.next = left;
+            left = left.next;
+            temp = temp.next;
+        }
+        while (right != null) {
+            temp.next = right;
+            right = right.next;
+            temp = temp.next;
+        }
+        return rs.next;
+    }
+}
